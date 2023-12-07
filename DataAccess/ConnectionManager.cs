96a08c527608ca8257b22f6ca7397be3fb700bd0
Valentin Morişcă <516431@student.fontys.nl>
2023-12-07 06:50:20 +0100

    .

---
+++ DataAccess/ConnectionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class ConnectionManager
+    {
+        public static string GetConnectionString()
+        {
+            return "Server=mssqlstud.fhict.local;Database=dbi516431;User Id=dbi516431;Password=Un1v3rs1t4t324@";
+        }
+    }
+}
